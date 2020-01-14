5702777dd52f1554303c3cc4f988c0f3406dfd2d
Frédéric Lesueurs <frederic.lesueurs@gmail.com>
2020-01-14 18:52:49 +0100

    first commit

---
+++ src/YoutubeDownloader.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+
+class YoutubeDownloader {
+
+    private $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+
+
+
+}
